c4a5131eede9e4f10426717fda9fc46db9424100
haoxoinyu <1014590194@qq.com>
2024-05-13 18:57:14 +0800

    后台页面修改+增加验证

--- application/common/validate/News.php
+++ application/common/validate/News.php
@@ -5,6 +5,7 @@ use think\Validate;
 class News extends Validate
 {
     protected $rule = [
+        'title' => 'require',
         'content'  => 'require',
         'creat_time'  => 'require'     
     ];
